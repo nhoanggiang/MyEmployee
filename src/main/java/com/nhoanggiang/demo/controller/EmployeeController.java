849d099d1322b2a09a0ec17cee544ed574ce8183
Nguyen Hoang Giang <hoanggiangd19bk@gmail.com>
2018-11-29 02:31:11 +0700

    updated config file

--- src/main/java/com/nhoanggiang/demo/controller/EmployeeController.java
+++ src/main/java/com/nhoanggiang/demo/controller/EmployeeController.java
@@ -21,45 +21,45 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeService employeeService;
 	
-	@GetMapping("/employee")
+	@GetMapping("/")
 	public String index(Model model) {
 		model.addAttribute("employees", employeeService.findAll());
 		return "list";
 	}
 	
-	@GetMapping("/employee/create")
+	@GetMapping("/create")
 	public String create(Model model) {
 		model.addAttribute("employee", new Employee());
 		return "form";
 	}
 	
-	@GetMapping("/employee/{id}/edit")
+	@GetMapping("/{id}/edit")
 	public String edit(@PathVariable int id, Model model) {
 		model.addAttribute("employee", employeeService.findOne(id));
 		return "form";
 	}
 	
-	@PostMapping("/employee/save")
+	@PostMapping("/save")
 	public String save(@Valid Employee employee, BindingResult result, RedirectAttributes redirect) {
 		if(result.hasErrors()) {
 			return "form";
 		}
 		employeeService.save(employee);
 		redirect.addFlashAttribute("success", "Saved employee successfully!");
-		return "redirect:/employee";
+		return "redirect:/";
 	}
 	
-	@GetMapping("/employee/{id}/delete")
+	@GetMapping("/{id}/delete")
 	public String delete(@PathVariable int id, RedirectAttributes redirect) {
 		employeeService.delete(id);
 		redirect.addFlashAttribute("success", "Deleted employee successfully!");
-		return "redirect:/employee";
+		return "redirect:/";
 	}
 	
-	@GetMapping("/employee/search")
+	@GetMapping("/search")
 	public String search(@RequestParam String s, Model model) {
 		if(s.equals("")) {
-			return "redirect:/employee";
+			return "redirect:/";
 		}
 		model.addAttribute("employees", employeeService.search(s));
 		return "list";
